290de7f4f7be1c3d614cd20d669ab0ba709fb076
Rohit <rohit@uttarainfo.com>
2018-04-19 16:42:31 +0530

    initial commit

---
+++ src/main/java/com/uttara/demoapp/DemoAppApplication.java
@@ -0,0 +1,24 @@
+package com.uttara.demoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppApplication.class, args);
+	}
+}
+
+
+@RestController
+class TestController{
+	//test jenkins
+	@GetMapping("/")
+	public String sayHello(){
+		return "Hello Spring cloud!!!!";
+	}
+}
